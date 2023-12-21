35e4ec547b933246c5c2446b4199c48cbb47687e
Jordi Planas - Enti <117847067+jordiiplanas@users.noreply.github.com>
2023-12-21 17:07:44 +0100

    Animaciones y escalas del player funcionales

--- 1942/source/Player.h
+++ 1942/source/Player.h
@@ -14,8 +14,8 @@ private:
 	bool isDying = false;
 
 public:
-	Player(Vector2 position) : GameObject(Vector2(100, 64)) {
-		renderers.emplace("idle", new ImageRenderer(transform, Vector2(4, 9), Vector2(25, 16)));
+	Player(Vector2 position) : GameObject(Vector2(100, 100)) {
+		renderers.emplace("idle", new ImageRenderer(transform, Vector2(0,0), Vector2(32, 32)));
 		std::vector<Vector2> rightDeltas
 		{
 			Vector2(32,0),Vector2(32 * 3,0),Vector2(32 * 5,0)
@@ -29,9 +29,9 @@ public:
 			Vector2(0,0), Vector2(32,0), Vector2(64,0), Vector2(96,0), Vector2(128,0), Vector2(160,0)
 		};
 
-		renderers.emplace("right", new AnimatedImageRenderer(transform, Vector2(4, 9), Vector2(25, 16), rightDeltas, false, 20));
-		renderers.emplace("left", new AnimatedImageRenderer(transform, Vector2(4, 9), Vector2(25, 16), leftDeltas, false, 20));
-		renderers.emplace("death", new AnimatedImageRenderer(transform, Vector2(0,112), Vector2(32, 32), deathDeltas, false, 5));
+		renderers.emplace("right", new AnimatedImageRenderer(transform, Vector2(0, 0), Vector2(32, 32), rightDeltas, false, 20));
+		renderers.emplace("left", new AnimatedImageRenderer(transform, Vector2(0, 0), Vector2(32, 32), leftDeltas, false, 20));
+		renderers.emplace("death", new AnimatedImageRenderer(transform, Vector2(0,112), Vector2(32, 32), deathDeltas, false, 20));
 		renderer = renderers["idle"];
 		rigidbody->SetLinearDrag(10);
 		SetScale(Vector2(0.5, 0.5));
