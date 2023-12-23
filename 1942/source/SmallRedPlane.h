e240128078132930a53b8ace1c95cd7af20fb361
AjandroMLG <alejandro.martinez.membrilla@enti.cat>
2023-12-23 16:14:56 +0100

    mIRACOMOGiraQueArteCOoÑo

--- 1942/source/SmallRedPlane.h
+++ 1942/source/SmallRedPlane.h
@@ -6,6 +6,11 @@ class SmallRedPlane : public Enemy
 protected:
 	bool goesRight;
 	bool isSpinning = false;
+	Vector2 direction;
+	Vector2 perpenDirection = Vector2();
+	float lastChangeTime;
+	float actualTime;
+
 	Vector2 CalculateTangentialVector(float radius, float angularSpeed, float dt)
 	{
 		float x = -radius * angularSpeed * sin(angularSpeed * dt);
@@ -16,14 +21,35 @@ public:
 	SmallRedPlane(Transform* transformPlayer, bool goesRight) :
 	 goesRight(goesRight), Enemy(1, 100, transformPlayer) 
 	{ 
+		lastChangeTime = SDL_GetTicks();
+		transform->position = Vector2(250, 250);
 		GetRigidbody()->SetLinearDrag(2);
+		if (goesRight)
+			direction = Vector2(5, 0);
+		else
+			direction = Vector2(-5, 0);
 	}
 
 	void Update(float dt) override 
 	{
-		Enemy::Update(dt);
-		GetRigidbody()->AddForce(CalculateTangentialVector(10,2,dt));
-		GetRigidbody()->AddForce(Vector2(0,10));
+		actualTime = SDL_GetTicks();
+		Enemy::Update(dt);		
+
+		if (actualTime - lastChangeTime > 1000)
+		{
+
+			perpenDirection = Vector2(-direction.y, direction.x);
+			lastChangeTime = SDL_GetTicks();
+			direction = perpenDirection;
+
+		}
+		GetRigidbody()->AddForce(direction + perpenDirection);
+		GetRigidbody()->AddTorque(4);
+
+
+		/*GetRigidbody()->AddForce(CalculateTangentialVector(10,2,dt));
+		GetRigidbody()->AddForce(Vector2(0,10));*/
+
 	}
 	void UpdateMovementPattern(float dt) override {}
 };
